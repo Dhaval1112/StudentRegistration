a6a89b1e26f4eaa27a2e631963ab4c14bc63349f
JDBrothers1016 <jp1016charusat@gmail.com>
2022-03-02 23:01:30 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using StudentRegistration.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult StudentSignup()
+        {
+            List<string> Courses = new List<string>();
+            Courses.Add("BCA");
+            Courses.Add("MCA");
+            Courses.Add("MSC.IT");
+            ViewBag.dropDown = new SelectList(Courses);
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult StudentSignup(StudentModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+                TempData["First_name"] = model.FirstName;
+                TempData["Last_name"] = model.LastName;
+                TempData["Mobile_number"] = model.MobileNumber;
+                TempData["Course"] = model.Course;
+                TempData["City"] = model.City;
+                TempData["EmailId"] = model.EmailId;
+                TempData["Fees"] = model.Fees;
+                TempData["Gender"] = model.Gender;
+
+            }    
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistration.Models
+{
+    public class StudentModel
+    {
+        [Required]
+        [Display(Name = "First name ")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name ")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Addres")]
+        public string EmailId { get; set; }
+
+        [Required]
+        [Display(Name = "Mobile Number")]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Course")]
+        public string Course { get; set; }
+
+        [Required]
+        [Display(Name = "Fees")]
+        public string Fees { get; set; }
+
+        [Required]
+        [Display(Name = "Tax")]
+        public string Tax { get; set; }
+    }
+}
